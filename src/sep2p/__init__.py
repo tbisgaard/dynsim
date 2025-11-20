93b192a67c9f257592fc62bbc1f9e90d61ae64fc
tbisgaard <biss3n@hotmail.com>
2025-11-20 16:39:11 +0100

    update imports

--- src/sep2p/__init__.py
+++ src/sep2p/__init__.py
@@ -6,22 +6,31 @@ Created on Thu Oct  9 12:53:12 2025
 """
 
 # Re-export public API from submodules
-from sep2p import utils
-from sep2p import pure_comp_prop
-from sep2p import mixture_prop
-from sep2p import data_loader
-from sep2p import system_parameters
 from sep2p import activity_coefficient_models
-from sep2p import equilibrium_calculations
 from sep2p import column_models
+from sep2p import data_loader
+from sep2p import equilibrium_calculations
+from sep2p import mixture_prop
 from sep2p import phys_constants
 from sep2p import plots
+from sep2p import pure_comp_prop
+from sep2p import system_parameters
+from sep2p import utils
+
 #from .models import User
 
 # Optional metadata
 __version__ = "0.0.1"
 __author__ = "Thomas Bisgaard Astrup"
 
-# Define what gets imported with: from my_project import *
-__all__ = ["add", "User"]
-
+# Define what gets imported with: from sep2p import *
+__all__ = ["activity_coefficient_models",
+           "column_models",
+            "data_loader",
+            "equilibrium_calculations",
+            "mixture_prop",
+            "phys_constants",
+            "plots",
+            "pure_comp_prop",
+            "system_parameters",
+            "utils"]
